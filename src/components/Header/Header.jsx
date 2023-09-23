92ef076b612223de1acd4ad5c972556acccd7cbc
Barshon <barshonweb@gmail.com>
2023-09-24 00:03:17 +0600

    responsive common navbar complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,31 +3,73 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-      <div className="flex justify-between">
+      <div className="flex justify-between ">
         <div className="">
-          <h3 className="text-xl md:text-3xl text-emerald-600 italic font-bold ">
-            Job Club
+          <h3 className="text-2xl md:text-3xl text-emerald-600 italic font-extrabold ">
+            <span className="text-indigo-700">Job</span>Club
           </h3>
         </div>
         <div className="order-first md:order-1 flex flex-col md:flex-row">
           <div>
-            <Link to="/" className="text-sm text-lime-600 mr-5">
+            <Link to="/" className="text-lg md:text-sm text-lime-600 mr-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4 inline md:hidden"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
               Home
             </Link>
           </div>
           <div>
-            <Link to="/stat" className="mr-5 text-sm text-lime-600">
+            <Link
+              to="/stat"
+              className="text-lg md:text-sm mr-5 text-sm text-lime-600"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4 inline md:hidden"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
               Statistics
             </Link>
           </div>
           <div>
-            <Link to="/applied" className="text-sm text-lime-600">
+            <Link
+              to="/applied"
+              className="text-lg md:text-sm text-sm text-lime-600"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4 inline md:hidden"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
               Applied Jobs
             </Link>
           </div>
         </div>
         <div className="order-2">
-          <button className="bg-lime-600 p-2 rounded-lg text-white hover:bg-lime-800">
+          <button className="bg-lime-600 p-1 md:p-2 rounded-lg text-white text-xs hover:bg-lime-800">
             Start Applying
           </button>
         </div>
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
-      <div className="mx-5 md:mx-36 mt-5">
+      <div className="mx-3 md:mx-36 mt-5">
         <Header></Header>
         <Outlet></Outlet>
       </div>
