180cd95018b44ea9f3e0f03fcc09c18c8d7291cc
Barshon <barshonweb@gmail.com>
2023-09-24 12:20:01 +0600

    y scroll always visible

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div className='mt-28'>
+            <p>Categories</p>
+        </div>
+    );
+};
+
+export default Categories;
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -4,9 +4,11 @@ import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
-      <div className="mx-3 md:mx-36 mt-5 ">
-        <Header></Header>
-        <Outlet></Outlet>
+      <div className='overflow-y-scroll'>
+        <div className="mx-3 md:mx-36 mt-5 ">
+          <Header></Header>
+          <Outlet></Outlet>
+        </div>
       </div>
     );
 };
