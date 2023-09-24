832c7c50295abb0b699b85eb29c347292a49e7c1
Barshon <barshonweb@gmail.com>
2023-09-24 12:33:13 +0600

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='text-center bg-black text-white p-10 '>
+        <div className='text-center bg-black text-white p-10 mt-10 '>
             <p>Job Club. All rights reserved. 2023</p>
         </div>
     );
