9b05ca6c2b3383a959dbd45ee70f58efbdfbcc26
Barshon <barshonweb@gmail.com>
2023-09-24 12:23:50 +0600

    y scroll always visible removed

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const Main = () => {
     return (
-      <div className='overflow-y-scroll'>
+      <div className=''>
         <div className="mx-3 md:mx-36 mt-5 ">
           <Header></Header>
           <Outlet></Outlet>
+          <Footer></Footer>
         </div>
       </div>
     );
