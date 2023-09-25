b9c0146571219b23a7f8940a537705eeae4736a1
Barshon <barshonweb@gmail.com>
2023-09-25 11:36:01 +0600

    each job boxes with infos created responsive

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Job from '../Job/Job';
+import { data } from 'autoprefixer';
 
 const Jobs = () => {
     const [jobs,setJobs]=useState([]);
@@ -10,12 +11,23 @@ const Jobs = () => {
         .then (data=> setJobs(data.slice(0,4)))
     },[])
     console.log(jobs);
+
+    const handleAllJobs=()=>{
+        
+    }
     return (
-        <div className='mt-48 grid grid-cols-2 gap-10 mx-32'>
-            {
-                jobs.map(job=><Job key={job.id} job={job} ></Job>)
-            }
+      <div>
+        <div className="md:mt-32 mt-10 grid md:grid-cols-2 gap-10 md:mx-32">
+          {jobs.map((job) => (
+            <Job key={job.id} job={job}></Job>
+          ))}
         </div>
+        <div className="text-center mt-5">
+          <button className="px-3 py-1 hover:bg-lime-800 rounded text-white bg-lime-600">
+            See All Jobs
+          </button>
+        </div>
+      </div>
     );
 };
 
