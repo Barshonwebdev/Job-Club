abf210e4dfa30998da844949d81e58f4b181b276
Barshon <barshonweb@gmail.com>
2023-09-26 17:58:52 +0600

    sort buttons added

--- src/components/Applied/Applied.jsx
+++ src/components/Applied/Applied.jsx
@@ -9,9 +9,14 @@ const Applied = () => {
     const [jobs,setJobs]=useState(appliedJobs);
     console.log(jobs);
     return (
-      <div className="mb-16">
+      <div className="mb-10">
         <h3 className="text-center mt-20 font-mono text-2xl">Applied Jobs</h3>
-        <div className='mt-20 '>
+        <div className='mt-10 flex justify-center md:justify-end text-sm items-center'>
+          <h2>Sort by: </h2>
+          <button className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Onsite</button>
+          <button className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Remote</button>
+        </div>
+        <div className='mt-5 '>
           {jobs.map((job) => (
             <AppliedJob key={job.id} job={job}></AppliedJob> 
           ))}
