131269a37a804aeb4193995a8b6ca4207f2661f2
Barshon <barshonweb@gmail.com>
2023-09-26 19:51:40 +0600

    sorting functionality added

--- src/components/Applied/Applied.jsx
+++ src/components/Applied/Applied.jsx
@@ -7,16 +7,28 @@ import AppliedJob from '../AppliedJob/AppliedJob';
 const Applied = () => {
     const appliedJobs= useLoaderData();
     const [jobs,setJobs]=useState(appliedJobs);
-    const [remoteJobs,setRemotejobs]=useState([]);
-    const [onsiteJobs,setOnsiteJobs]=useState([]);
-    console.log(jobs);
+    console.log(appliedJobs);
+    const handleRemote=()=>{
+      const remotejobArray = jobs.filter((job) => job.jobLocation == "Remote");
+      setJobs(remotejobArray);
+    }
+    const handleOnsite=()=>{
+      const onsitejobArray = jobs.filter((job) => job.jobLocation == "Onsite");
+      setJobs(onsitejobArray);
+    }
+
+    const handleUnsorted=()=>{
+      setJobs(appliedJobs);
+    }
+    
     return (
       <div className="mb-10">
         <h3 className="text-center mt-20 font-mono text-2xl">Applied Jobs</h3>
         <div className='mt-10 flex justify-center md:justify-end text-sm items-center'>
           <h2>Sort by: </h2>
-          <button className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Onsite</button>
-          <button className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Remote</button>
+          <button onClick={handleOnsite} className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Onsite</button>
+          <button onClick={handleRemote}  className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Remote</button>
+          <button onClick={handleUnsorted}  className='ms-2 bg-sky-600 hover:bg-sky-800 text-white p-1 rounded-lg'>Unsorted</button>
         </div>
         <div className='mt-5 '>
           {jobs.map((job) => (
