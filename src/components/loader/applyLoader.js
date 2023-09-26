fe720ab956f6cbcf4908d33776d341e30585141f
Barshon <barshonweb@gmail.com>
2023-09-26 15:14:52 +0600

    all data displayed on ui

--- src/components/loader/applyLoader.js
+++ src/components/loader/applyLoader.js
@@ -1,9 +1,19 @@
+import { getStoredJobs } from "../../utilities/fakedb";
 
 const applyLoader= async ()=>{
     const loadedJobs=await fetch('jobs.json');
     const jobs= await loadedJobs.json();
     console.log(jobs);
-    return jobs;
+    const storedJobs=getStoredJobs();
+    const appliedJobs=[];
+    
+    for (const id in storedJobs){
+        const addedJobs=jobs.find(job=>job.id==id);
+        appliedJobs.push(addedJobs);
+    }
+    console.log(appliedJobs);
+    return appliedJobs;
+
 }
 
 export default applyLoader;
